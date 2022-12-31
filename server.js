3924b45a25f8c3cc596af721fa6a6b453428d00b
Sonmung <sonmung@flexagn.com>
2022-12-31 15:56:56 +0530

    removed deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 5000;
 const io = require("socket.io")(PORT, {
     cors: {
-        origin: "http://chatapp-env.eba-kpihd2kd.ap-northeast-1.elasticbeanstalk.com/"
+        origin: "http://localhost:3000"
     }
 });
 io.on('connection', socket => {
